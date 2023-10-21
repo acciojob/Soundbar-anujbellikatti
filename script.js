61f2d3c90c001ce0739bcd02d9402de6f5ccb2b0
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-21 15:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+let audio;
+
+function playSound(soundFileName) {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+    
+    audio = new Audio(`sounds/${soundFileName}`);
+    audio.play();
+}
+
+function stopSound() {
+    if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    }
+}
